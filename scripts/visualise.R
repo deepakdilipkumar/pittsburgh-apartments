bf5996e8c40634e33f64dd87b4e0fc0d51b72777
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-05-26 22:06:03 +0530

    Adding visualtion code

---
+++ scripts/visualise.R
@@ -0,0 +1,29 @@
+library(ggmap)
+
+apt <- read.csv('cleanedaptdata.csv', head=T) #,colClasses=c("character","character","character","character","character","character","character","character","character","character","character","character","numeric","numeric"))
+
+latFactor <- factor(apt$Latitude)
+lonFactor <- factor(apt$Longitude)
+apt$Latitude <- as.numeric(levels(latFactor))[latFactor]		#Convert from character to numeric
+apt$Longitude <- as.numeric(levels(lonFactor))[lonFactor]
+
+apt$Longitude
+apt$Latitude
+
+cmu <- geocode("Carnegie Mellon University")
+cmulong <- cmu[1]
+cmulat <- cmu[2]
+
+pitsmap <- qmap("Carnegie Mellon University",zoom=14,source="google",type="roadmap")
+
+pdf(file="Apartments.pdf")
+pitsmap + 
+geom_point(aes(x = Longitude, y = Latitude), data = apt) +
+geom_point(aes(x=lon, y=lat, color="red"), data=cmu)
+dev.off()
+
+pdf(file="By Rent.pdf")
+pitsmap + 
+geom_point(aes(x = Longitude, y = Latitude, size =RentPerPerson), data = apt) +
+geom_point(aes(x=lon, y=lat, color="red"), data=cmu)
+dev.off()
