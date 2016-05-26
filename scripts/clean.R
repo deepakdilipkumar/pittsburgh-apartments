4e3cf32c1a456376afd6cc697234cb201e32b484
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-05-26 21:49:33 +0530

    Cleaned up data

--- scripts/clean.R
+++ scripts/clean.R
@@ -1,4 +1,5 @@
 library(readxl)
+library(ggmap)
 
 apt <- read_excel("apartments.xlsx")
 names(apt)
@@ -7,7 +8,12 @@ names(apt)
 # [5] "Sublet/Lease extension?"   "Full address"             
 # [7] "Locality"                  "No. of bedrooms available"
 # [9] "No. of tenants reqd."      "Floor"                    
-# [11] "Rent"                      "Available from"           
-# [13] "Available until"           "Looking for:"             
-# [15] "Other comments " 
-relevant <- apt[,c(5,7,8,9,10,11,12,13,14,15)]
+#[11] "Rent"                      "RentPerPerson"            
+#[13] "Utlities"                  "Available from"           
+#[15] "Available until"           "Looking for:"             
+#[17] "Other comments " 
+relevantCols <- apt[1:69,c(5,6,7,8,9,10,11,12,13,14,15,16,17)]
+locs <- geocode(relevantCols[,2])
+final <- data.frame(relevantCols,locs)
+names(final) <- c("SubletLease","Address","Locality","Bedrooms","Tenants","Floor","Rent","RentPerPerson","Utlities","From","Until","Gender","Comments","Longitude","Latitude")
+write.csv(final,"cleanedaptdata.csv")
